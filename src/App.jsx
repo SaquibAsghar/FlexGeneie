0b4d15613868630dc446c8fe9cbbcf4b5cca5582
Saquib Asghar <saq9516@gmail.com>
2024-08-14 23:33:49 +0530

    batman commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="text-xl text-[#d6d6d6]">
+        <p>Welcome to the FlixGenie</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
